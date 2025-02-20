bafc5dd56b552ae8bf77a612e00b281b639581cb
Yu <18972780275@163.com>
2025-02-20 09:40:27 +0800

    modify arm make

--- modules/robot-dog/scripts/build.arm64.sh
+++ modules/robot-dog/scripts/build.arm64.sh
@@ -1,5 +1,5 @@
 cp make/CMakeLists.txt.arm64 CMakeLists.txt
 mkdir build
 cd build
-cmake -DROS_ENABLE=OFF -DROS2_ENABLE=OFF -DLCM_ENABLE=OFF -DDDS_ENABLE=ON -DGLOG_ENABLE=ON  -DCMAKE_BUILD_TYPE=Release ../
+cmake -DROS_ENABLE=ON -DROS2_ENABLE=OFF -DLCM_ENABLE=OFF -DDDS_ENABLE=OFF -DGLOG_ENABLE=ON  -DCMAKE_BUILD_TYPE=Release ../
 make -j$(nproc)
